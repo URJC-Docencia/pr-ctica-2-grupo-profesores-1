d3d716bb35ff5eb3754d9dfe807125776f59b552
idhunita <javier_01_gl@hotmail.com>
2023-10-31 12:35:05 +0100

    PrimeraParte

--- src/Frecuencias.java
+++ src/Frecuencias.java
@@ -9,6 +9,7 @@ import java.util.*;
  * @author mayte
  */
 public class Frecuencias {
+
     /**
      *
      * @param l
@@ -16,14 +17,36 @@ public class Frecuencias {
      * @return
      */
     public List<Integer> kMaximunFrecuencies(List<Integer> l, int k){
-        throw new RuntimeException("Not implemented yet");
+        Map<Integer, Integer> frec = new HashMap<>();
+
+        for (Integer dato : l){
+            if (frec.containsKey(dato)){
+                int n = frec.get(dato);
+                frec.put(dato, n + 1);
+            }else{
+                frec.put(dato, 1);
+            }
+        }
+
+        Set<Map.Entry<Integer, Integer>> entrySet =  frec.entrySet();
+        List entryList = Arrays.asList(entrySet.toArray());
+        Collections.sort(entryList,  new SortbyOcurrences());
+
+        List<Integer> returnList = new LinkedList<>();
+
+        for(int i=0; i<k; i++){
+            Map.Entry<Integer, Integer> auxEntry = (Map.Entry<Integer, Integer>) entryList.get(i);
+            returnList.add(0, auxEntry.getKey());
+        }
+
+        return returnList;
     }
 
     private class SortbyOcurrences implements Comparator<Map.Entry<Integer, Integer>>
     {
         @Override
         public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-            throw new RuntimeException("Not implemented yet");
+            return o2.getValue() - o1.getValue();
         }
     }
 }
